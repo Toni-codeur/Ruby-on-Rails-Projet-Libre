129b24f2f6793387e1fb15972c8492f5c8a75fe5
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-19 14:16:40 +0100

    Stylisation du profil des utilisateurs | Close #4

--- app/helpers/utilisateurs_helper.rb
+++ app/helpers/utilisateurs_helper.rb
@@ -1,2 +1,7 @@
 module UtilisateursHelper
+  def gravatar_for(utilisateur)
+    gravatar_id = Digest::MD5::hexdigest(utilisateur.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: utilisateur.nom, class: "gravatar")
+  end
 end
