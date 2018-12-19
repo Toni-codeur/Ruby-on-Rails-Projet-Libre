e0b74aafbf77467f298ec93e99af029e36d6f661
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-19 14:53:59 +0100

    Ajout des messages d'erreurs a notre formulaire | Close #6

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -6,4 +6,26 @@ class UtilisateursController < ApplicationController
   def new
     @utilisateur = Utilisateur.new
   end
-end
+
+  def create
+    @utilisateur = Utilisateur.new(utilisateur_params)
+    if @utilisateur.save
+      #Sauvgarde confirmé <- true
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def utilisateur_params
+      params.require(:utilisateur).permit(
+        :nom, 
+        :email, 
+        :password, 
+        :password_confirmation
+      )
+
+
+    end
+  end
