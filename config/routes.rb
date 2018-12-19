2cf32f5ec36cd10c73b74313566ae75b319efb4d
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-19 16:19:09 +0100

    Creation d'un controller pour nos connexions et deco | Close #10

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'statique#accueil'
   get '/contact', to: "statique#contact"
   get '/team', to: "statique#team"
   get '/services', to: "statique#services"
   get '/signup', to: "utilisateurs#new"
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :utilisateurs
 
 end
