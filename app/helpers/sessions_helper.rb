96ecb1092b73ea6bb137314e9561939e6a77612b
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-20 11:06:35 +0100

    Modification du menu principal | Close #14

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,8 @@ module SessionsHelper
   def current_utilisateur
     @current_utilistaeur ||= Utilisateur.find_by(id: session[:utilisateur_id])
   end
+
+  def logged_in?
+    !current_utilisateur.nil?
+  end
 end
