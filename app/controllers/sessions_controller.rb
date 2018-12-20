ae24fc5dc8e48149c77e2a26af07d52dc9a5c11d
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-20 10:42:20 +0100

    Creation d'un formulaire de connexion | Close #11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    utilisateur = Utilisateur.find_by(email: params[:session][:email].downcase)
+    if utilisateur && utilisateur.authenticate(params[:session][:password])
+    
+    else
+      # Message d'erreur
+      flash.now[:danger] = "E-mail ou mot de passe invalide !"
+      render "new"
+    end
+  end
+
+  def destroy
+
   end
 end
