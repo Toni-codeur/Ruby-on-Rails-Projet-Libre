8d25f984dcefd59acd99df5c7f352cd3c31e45ad
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-20 23:21:13 +0100

    Creation du token pour les compte utilisateurs | Close #25

--- app/models/utilisateur.rb
+++ app/models/utilisateur.rb
@@ -1,6 +1,7 @@
 class Utilisateur < ApplicationRecord
-  attr_accessor :remember_token
-  before_save {self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :nom, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
@@ -8,6 +9,12 @@ class Utilisateur < ApplicationRecord
               uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+  def Utilisateur.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
   def Utilisateur.new_token
     SecureRandom.urlsafe_base64
@@ -25,5 +32,16 @@ class Utilisateur < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = Utilisateur.new_token
+    self.activation_digest = Utilisateur.digest(activation_token)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181220140736) do
+ActiveRecord::Schema.define(version: 20181220215729) do
 
   create_table "utilisateurs", force: :cascade do |t|
     t.string "nom"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20181220140736) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_utilisateurs_on_email", unique: true
   end
 
