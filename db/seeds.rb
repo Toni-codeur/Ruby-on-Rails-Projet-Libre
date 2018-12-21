ca4da5102635e2b38efa99b2df6e98139c1f962e
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-21 11:45:08 +0100

    test debug 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-Utilisateur.create!(nom: "Alex",
-                    email: "alexisolland@getcodingknowledge.com",
-                    password: "root1234",
-                    password_confirmation: "root1234",
+Utilisateur.create!(nom: "Toni",
+                    email: "jonathan.grondin@yahoo.fr",
+                    password: "test69",
+                    password_confirmation: "test69",
                     admin: true,
                     activated: true,
                     activated_at: Time.zone.now)
