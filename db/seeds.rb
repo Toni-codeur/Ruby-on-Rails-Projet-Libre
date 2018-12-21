8cf55c761cf5b1046be63dd0ab87b6818e774f59
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-21 11:21:57 +0100

    Test2 debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-Utilisateur.create!(nom: "Toni",
-                    email: "admin@admin.fr",
-                    password: "test69",
-                    password_confirmation: "test69",
+Utilisateur.create!(nom: "Alex",
+                    email: "alexisolland@getcodingknowledge.com",
+                    password: "root1234",
+                    password_confirmation: "root1234",
                     admin: true,
                     activated: true,
                     activated_at: Time.zone.now)
 
 59.times do |n|
   nom = Faker::Name.name
-  email = "example-#{n + 1}@marvel.com"
+  email = "example-#{n + 1}@getcodingknowledge.com"
   password = "password"
   Utilisateur.create!(nom: nom,
                       email: email,
