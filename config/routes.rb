e8ec84e2ce2b3cf74436f7d0b93bc88fa4f584de
Toni-codeur <jonathan.grondin@yahoo.fr>
2018-12-21 12:58:01 +0100

    Controller pour la reinitialisation de nos mots de passe | Close #29

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get "sessions/new"
 
   root "statique#accueil"
@@ -11,4 +15,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   resources :utilisateurs
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
